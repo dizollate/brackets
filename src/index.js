696deab3467fc523f2a9745732f2bf356a6bc8ad
dizollate <66797250+dizollate@users.noreply.github.com>
2021-03-03 12:19:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let string = str;
+  const pairsBrackets = bracketsConfig.map(([open, close]) => {
+    if(isNaN(+open)) {
+      return `\\${open}\\${close}`;
+      }
+    
+    return `${open}${close}`;
+});
+  while(string.length) {
+    let lengthBef = string.length;
+    let lengthAf;
+    
+    for(let value of pairsBrackets){
+      const reg = new RegExp(value, flags:'g');
+      string = string.replace(reg, "");
+      lengthAf = string.length;
+      }
+    if(lengthBef === lengthAf) break;
+    }
+  return string.length === 0;
+} 
